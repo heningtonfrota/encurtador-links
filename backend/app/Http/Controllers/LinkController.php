7f5b0b49eb0738c6cb35992af695d3f2f58c469f
heningtonfrota <henington.frota@systock.com.br>
2023-08-24 16:45:09 -0400

    migation link

--- backend/app/Http/Controllers/LinkController.php
+++ backend/app/Http/Controllers/LinkController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreLinkRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -24,8 +25,9 @@ class LinkController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreLinkRequest $request)
     {
+        \Log::info($request->all());
         return response()->json([], Response::HTTP_OK);
     }
 
--- backend/app/Http/Requests/StoreLinkRequest.php
+++ backend/app/Http/Requests/StoreLinkRequest.php
@@ -13,7 +13,7 @@ class StoreLinkRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
